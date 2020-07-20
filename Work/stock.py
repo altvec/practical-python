2a58bfcaf23930a5896ed14c9e36ec5e86051d24
altvec <me@sergeykalistratov.com>
2020-07-20 23:32:43 +0500

    Done 7.8 and 7.9

--- Work/stock.py
+++ Work/stock.py
@@ -1,15 +1,19 @@
-from typedproperty import typedproperty
+from typedproperty import typedproperty, String, Integer, Float
 
 class Stock:
     '''An instance of a stock holding consisting of name, shares, and price.'''
-    name = typedproperty('name', str)
-    shares = typedproperty('shares', int)
-    price = typedproperty('price', float)
+    name = String('name')
+    shares = Integer('shares')
+    price = Float('price')
 
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+    
+    def __repr__(self):
+        # self.name!r == repr(self.name)
+        return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
 
     @property
     def cost(self):
@@ -19,7 +23,3 @@ class Stock:
     def sell(self, amnt):
         '''Sell a number of shares.'''
         self.shares -= amnt
-
-    def __repr__(self):
-        # self.name!r == repr(self.name)
-        return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
